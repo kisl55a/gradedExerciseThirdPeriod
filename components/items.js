423a5ae3fffd5e2a345abd2df54c1322ebec6839
dmitrii <kislenko1910@mail.ru>
2020-02-12 20:54:25 +0200

    Items CRUD works now

--- components/items.js
+++ components/items.js
@@ -7,12 +7,77 @@ const basicStrategy = require('../middlwares/passportBasic');
 const jwtStrategy = require('../middlwares/passportJWT');
 const jwt = require('jsonwebtoken');
 const jwtSecretKey = require('../secretKey.json');
+var multer = require('multer')
+var upload = multer({ dest: '../uploads/' })
+fs = require('fs')
 
 router.get('/',
-jwtStrategy.authenticate('jwt', { session: false }),
-(req, res) => {
-    res.send(items.getAllItems(req.user.id))
-}
+    jwtStrategy.authenticate('jwt', { session: false }),
+    (req, res) => {
+        res.send(items.getAllItems(req.user.id))
+    }
 )
 
+router.post('/',
+    jwtStrategy.authenticate('jwt', { session: false }),
+    upload.array('images', 4),
+    (req, res) => {
+        let images = []
+        req.files.forEach((element, i) => {
+            fs.rename(req.files[i].path, './uploads/' + req.files[i].originalname, function (err) {
+                if (err) throw err;
+            });
+            images.push(req.files[i].originalname)
+        });
+        let newItem = {
+            id: items.getNewItemId(),
+            idUser: req.user.id,
+            ...req.body,
+            images
+        }
+        res.send(items.addNewItem(newItem))
+    }
+)
+
+router.put('/:id',
+    jwtStrategy.authenticate('jwt', { session: false }),
+    upload.array('images', 4),
+    (req, res) => {
+        console.log(req.files);
+        if (req.files !== undefined) {
+        let images = []
+        req.files.forEach((element, i) => {
+            fs.rename(req.files[i].path, './uploads/' + req.files[i].originalname, function (err) {
+                if (err) throw err;
+            });
+            images.push(req.files[i].originalname)
+        });
+             itemToEdit = {
+                id: parseInt(req.params.id),
+                idUser: req.user.id,
+                ...req.body,
+                images: images
+            }
+        } else {
+             itemToEdit = {
+                id: parseInt(req.params.id),
+                idUser: req.user.id,
+                ...req.body,
+            }
+        }
+
+        res.send(items.changeItem(itemToEdit))
+    }
+);
+
+router.delete('/:id', 
+jwtStrategy.authenticate('jwt', { session: false }),
+(req, res) => {
+    if(items.deleteItem(req.params.id, req.user.id)){
+        res.send(items.getAllItems(req.user.id))
+    } else {
+        res.status(400).send("No product with such an id");
+    }
+}
+)
 module.exports = router;
--- models/itemsModel.js
+++ models/itemsModel.js
@@ -1,48 +1,39 @@
 let itemsData = [
     {
-        id:1,
+        id: 1,
         idUser: 1,
         title: "Test title",
         description: "Some description",
-        Location: "Oulu",
-        images:["uploads/i1.png", "uploads/i2.png"],
+        location: "Oulu",
+        images: ["uploads/i1.png", "uploads/i2.png"],
         price: 15,
         date: "19.07.2020",
         deliveryType: "shipping",
-        contacts: {
-            name: "Dmitrii",
-            phone: +79226313400
-        }
+        contacts: "Dmitrii +79226313400"
     },
     {
-        id:2,
+        id: 2,
         idUser: 2,
         title: "Test title2",
         description: "Some description",
-        Location: "Oulu",
-        images:["uploads/i1.png", "uploads/i2.png"],
+        location: "Oulu",
+        images: ["uploads/i1.png", "uploads/i2.png"],
         price: 15,
         date: "19.07.2020",
         deliveryType: "shipping",
-        contacts: {
-            name: "Dmitrii",
-            phone: +79226313400
-        }
+        contacts: "Dmitrii +79226313400"
     },
     {
-        id:3,
+        id: 3,
         idUser: 1,
         title: "Test title 3",
         description: "Some description",
-        Location: "Oulu",
-        images:["uploads/i1.png", "uploads/i2.png"],
+        location: "Oulu",
+        images: ["uploads/i1.png", "uploads/i2.png"],
         price: 15,
         date: "19.07.2020",
         deliveryType: "shipping",
-        contacts: {
-            name: "Dmitrii",
-            phone: +79226313400
-        }
+        contacts: "Dmitrii +79226313400"
     }
 ]
 
@@ -54,31 +45,40 @@ module.exports = {
     getAllItems: (idUser) => {
         let result = [];
         itemsData.forEach(element => {
-            (idUser == element.idUser ? result.push(element): null )
-    })
-    return result;
-},
-
+            (idUser == element.idUser ? result.push(element) : null)
+        })
+        return result;
+    },
+    getNewItemId: () => {
+        return itemsData.length + 1;
+    },
     addNewItem: (item) => {
         itemsData.push(item)
         return item
     },
 
     changeItem: (item) => {
-        console.log('item: ', item);
         let result = null;
         itemsData.forEach((element, i) => {
-            if (element.id == item.id) {
-              itemsData[i] = { 
-                  id: item.id,
-                  ...item
+            if (element.id == item.id && element.idUser == item.idUser) {
+                itemsData[i] = {
+                    ...itemsData[i],
+                    ...item
                 }
-                console.log('itemsData[i]: ', itemsData[i]);
-                result = itemsData[i] ;
-                
-            } 
+                result = itemsData[i];
+            }
         });
         return result;
-        
+
+    },
+    deleteItem: (id, idUser) => {
+        let result = false;
+        itemsData.forEach((element, i) => {
+            if (element.id == id && element.idUser == idUser) {
+              itemsData.splice(i, 1);
+              result = true
+            }
+        });
+        return result;
     }
-  }
+}
